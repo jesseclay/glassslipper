4b3fddf96b11e21c279f57448c162ae9c8f3c01c
Michael Bernstein <msb@cs.stanford.edu>
2014-01-22 20:16:28 -0800

    basic node port of web site

--- app.js
+++ app.js
@@ -4,20 +4,21 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
+
+var routes = require('./routes');
+// Example route
+// var user = require('./routes/user');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
-
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -33,8 +34,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Add routes here
 app.get('/', routes.index);
-app.get('/users', user.list);
+// Example route
+// app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Intro to HCI' });
+  res.render('index');
 };

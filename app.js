bc8b01058a0f3bea7917fe12347da4edc939a724
vinamratasingal <girlrockingguna@gmail.com>
2014-02-17 14:01:29 -0800

    committing changes to backend

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ var about = require('./routes/about');
 
 // Example route
 // var user = require('./routes/user');
+var mongoose = require('mongoose');
+var configDB = require('./config/database.js'); 
+var flash = require('connect-flash'); 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var login= require('./routes/login'); 
+var logout = require('./routes/logout'); 
+var signup = require('./routes/signup'); 
 
 var app = express();
 
@@ -33,10 +41,25 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser('Intro HCI secret key'));
-app.use(express.session());
-// app.use(express.bodyParser());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.session());
+app.use(flash()); 
+app.use(express.logger());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+// app.use(express.bodyParser());
+
+
+//passsport stuff 
+//var Account = require('./models/account');
+//passport.use(new LocalStrategy(Account.authenticate()));
+//passport.serializeUser(Account.serializeUser());
+//passport.deserializeUser(Account.deserializeUser());
+
+
+//mongoose 
+mongoose.connect('mongodb://localhost/passport_local_mongoose');
 
 // development only
 if ('development' == app.get('env')) {
@@ -46,14 +69,24 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.landing);
 app.get('/login', login.view);
-app.get('/createAccount', createAccount.view);
 app.get('/search', index.search);
 app.get('/results', searchResults.showResults);
 app.get('/history', history.view);
 app.get('/favorites', favorites.view);
-app.get('/addShoe', addShoe.view);
 app.get('/about', about.view);
-app.get('/addShoe', addShoe.view); 
+app.get('/addShoe', addShoe.view);
+app.get('/signup', signup.view); 
+app.get('/logout', logout.logout); 
+app.post('/signup', signup.sendData); 
+app.post('/login', login.sendData); 
+
+/*function isLoggedIn(req, res, next) {
+	// if user is authenticated in the session, carry on 
+	if (req.isAuthenticated())
+		return next();
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+}*/
 
 // app.get('/test', test.view);
 // app.get('/project/:name/:image_url', project.viewProject);
--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,12 @@
+var passport = require('passport');
+require('../config/passport')(passport); 
+
 exports.view = function(req, res) {
-  res.render('login'); 
-}
+	res.render('login.handlebars', { message: req.flash('loginMessage') });
+};
+
+exports.sendData = passport.authenticate('local-login', {
+		successRedirect : '/', // redirect to the secure profile section
+		failureRedirect : '/login', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+});

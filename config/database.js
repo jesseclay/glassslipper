a8da8eaeba174d3cece6cd5ea2a2d8743c41b8e1
vinamratasingal <girlrockingguna@gmail.com>
2014-02-19 15:50:12 -0800

    Output URLs for database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 var local_uri = 'mongodb://localhost/test'; 
 var database_uri = process.env.MONGOLAB_URI || local_uri; 
+console.log(database_uri);
+console.log(process.env.MONGOLAB_URI);
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
 MongoClient.connect(database_uri, function (err, db) {

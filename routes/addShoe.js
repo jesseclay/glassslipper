278ab7242b20dc2ac820cd2656719b4fe532f502
Kai-Yuan Neo <kneo@stanford.edu>
2014-02-20 22:55:45 -0800

    made partials for menu bars

--- routes/addShoe.js
+++ routes/addShoe.js
@@ -1,3 +1,5 @@
 exports.view = function(req, res) {
-  res.render('addShoe'); 
+  res.render('addShoe', {
+  	'user': req.user,
+  }); 
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var shoe_data = require('../shoe_catalog.json');
 
 exports.landing = function(req, res) {
-  res.render('landing', { message: req.flash('log_out') });
+  res.render('landing', { 
+  	message: req.flash('log_out') ,
+  	'user': req.user 
+  });
 };
 
 exports.search = function(req, res) {

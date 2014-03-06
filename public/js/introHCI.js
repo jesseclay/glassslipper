4fe5e452d2dfa290912c32ce929c69ef9818adb4
Kai-Yuan Neo <kneo@stanford.edu>
2014-03-05 18:14:59 -0800

    added analytics code to record time between load and submit on search pages

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -13,10 +13,16 @@ function initializePage() {
 	* a) time from entry to submit
 	* b) number of submits in a session
 	*/
+
 	ga("send", "event", "pageLoad", "init");
+	var startTime = new Date().getTime();
 	$(".shoeSearchForm").submit(recordSubmit);
 
 	function recordSubmit() {
+		var endTime = new Date().getTime();
+		var timeSpent = endTime - startTime;
+		ga('send', 'event', 'userInteractionTime', 'submit', 'loadToSearchTime', timeSpent);
+		ga('send', 'timing', 'userInteractionTime', 'timeToSearch', timeSpent);
 		ga("send", "event", "searchSubmit", "submit");
 	}
 }
